c01dfb009e65e717b882a8410802eda0a3a96767
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-16 14:41:40 +0900

    記事の入力画面の作成完了

--- src/views/Bbs.vue
+++ src/views/Bbs.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
+    <div>
+      投稿者名：
+      <input type="text" v-model="articleName" /> <br />
+      投稿内容：
+      <textarea
+        name="articleContent"
+        v-model="articleContent"
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
+      <br>
+      <button type="button" v-on:click="addArticle">記事投稿</button>
+    </div>
+    <hr />
     <div v-for="article of currentArticleList" v-bind:key="article.id">
       投稿者名：{{ article.name }} <br />
       投稿内容：
       <pre>{{ article.content }}</pre>
+      <hr>
     </div>
   </div>
 </template>
@@ -15,6 +31,10 @@ import { Component, Vue } from "vue-property-decorator";
 export default class XXXComponent extends Vue {
   //現在の記事一覧
   private currentArticleList = new Array<Article>();
+  //投稿者名
+  private articleName = "";
+  //投稿内容
+  private articleContent = "";
 
   created(): void {
     this.currentArticleList = this.$store.getters.getArticles;
